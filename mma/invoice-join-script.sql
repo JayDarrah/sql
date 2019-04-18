961599544dbf5dd4c2ec4e6cca9e78ed810cc630
JayDarrah <jasondarrah@gmail.com>
2019-04-18 08:44:07 -0400

    Invoice join example

---
+++ mma/invoice-join-script.sql
@@ -0,0 +1,7 @@
+SELECT i.OrderNumber, i.OrderDate, i.CustomerName, p.Code as 'Item Code', p.Description, 
+	concat('$',format(p.Price,2)) as Price, l.Quantity, concat('$',format(i.Total,2)) as 'Total'
+		FROM lineitem l
+			INNER JOIN product p
+				ON l.ProductID = p.ID
+			INNER JOIN invoice i
+				ON	l.InvoiceID = i.ID;
