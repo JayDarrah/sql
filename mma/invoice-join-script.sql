9b68982e6924fe82810ffef6879b93417b8fb19a
JayDarrah <jasondarrah@gmail.com>
2019-04-18 09:14:22 -0400

    Just a quick update

--- mma/invoice-join-script.sql
+++ mma/invoice-join-script.sql
@@ -1,5 +1,5 @@
 SELECT i.OrderNumber, i.OrderDate, i.CustomerName, p.Code as 'Item Code', p.Description, 
-	concat('$',format(p.Price,2)) as Price, l.Quantity, concat('$',format(i.Total,2)) as 'Total'
+	concat('$',format(p.Price,2)) as "List Price", l.Quantity, concat('$',format(i.Total,2)) as 'Total'
 		FROM lineitem l
 			INNER JOIN product p
 				ON l.ProductID = p.ID
